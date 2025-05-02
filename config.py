3c30d9cb326eb84e74cfca16e12ff6632f1f8a2d
Piyushmeena11 <meenapiyush23@gmail.com>
2025-05-02 22:55:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "27041528"
+api_hash = "0a67358ebccb6a642778e3844f5da335"
+bot_token = "7377417550:AAFzuTaS2Rermg-iu6y_FSl6Mr9UXiZgzSg"
 auth_users = [6763842898]
