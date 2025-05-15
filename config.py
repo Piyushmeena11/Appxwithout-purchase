f9e0a5ef09ce1762798ef5c29fe413bb268bd5f4
Piyushmeena11 <meenapiyush23@gmail.com>
2025-05-15 17:15:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "27041528"
 api_hash = "0a67358ebccb6a642778e3844f5da335"
-bot_token = "7377417550:AAFzuTaS2Rermg-iu6y_FSl6Mr9UXiZgzSg"
+bot_token = "6425512691:AAHsxcLIKTEb4XhRh53VbINcKCd1RU1lnF4"
 auth_users = [6763842898]
